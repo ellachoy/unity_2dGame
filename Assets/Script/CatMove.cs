767baee030eb81d6cb46690ef7a6032dde522d3e
ellachoy <egal21de@gmail.com>
2024-10-07 12:15:17 +0200

    Implementiert dass das Object nach links,rechts und oben zu bewegen

---
+++ Assets/Script/CatMove.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatMove : MonoBehaviour
+{
+    Rigidbody2D rigid2D;
+    public float jumpPower;
+    public float moveDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //jump
+        if (Input.GetMouseButtonDown(0))
+        {
+            rigid2D.velocity = Vector2.up * jumpPower;
+        }
+
+        //move to right, left
+      
+        if (Input.GetMouseButtonUp(1)){
+            rigid2D.velocity = Vector2.right * moveDistance;
+        }
+        if (Input.GetMouseButtonUp(2)){
+            rigid2D.velocity = Vector2.left* moveDistance;
+        }
+    }
+}
